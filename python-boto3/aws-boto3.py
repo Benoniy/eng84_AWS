bedf7bd9482f0bc112e74bfc01922505c00e5ecb
Ben <benranson5@gmail.com>
2021-04-30 11:32:33 +0100

    completed boto3

--- python-boto3/aws-boto3.py
+++ python-boto3/aws-boto3.py
@@ -5,24 +5,43 @@ ak = file.readline().strip("\n")
 sak = file.readline().strip("\n")
 region = "eu-west-1"
 s3 = boto3.client('s3', aws_access_key_id=ak, aws_secret_access_key=sak, region_name=region)
+bucket = "eng84-ben-test-bucket"
 
 
 def create_bucket():
     location = {'LocationConstraint': region}
-    s3.create_bucket(Bucket="eng84-ben-test-bucket", CreateBucketConfiguration=location)
+    s3.create_bucket(Bucket=bucket, CreateBucketConfiguration=location)
 
 
 def list_all():
     list_b = s3.list_buckets()
 
     print("Buckets:")
-    for bucket in list_b['Buckets']:
-        print(f"{bucket['Name']}")
+    for buck in list_b['Buckets']:
+        print(f"{buck['Name']}")
 
 
 def delete_bucket():
-    pass
+    s3.delete_bucket(Bucket=bucket)
+
+
+def upload_file(filename):
+    s3.upload_file(filename, bucket, filename)
+
+
+def sync_file(filename):
+    s3.download_file(bucket, filename, filename)
+
+
+def delete_file(filename):
+    s3.delete_object(Bucket=bucket, Key=filename)
 
 
 if __name__ == "__main__":
-    list_all()
+    # create_bucket()
+    # list_all()
+    # upload_file("test.md")
+    # sync_file("test.md")
+    # delete_file("test.md")
+    # delete_bucket()
+    pass
